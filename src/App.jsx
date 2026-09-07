42507f9bb2dfe67ed4cf7b09a2c1071ba73571a9
Tushar Patel <pateltusharsa@gmail.com>
2026-09-07 11:34:53 +0530

    07-09-26

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Navbar  from "./Component/Navbar"
+import Studentdata from "./Component/Studentdata" 
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+    
+      <Studentdata/>
+    </>
+  )
+}
+
+export default App
